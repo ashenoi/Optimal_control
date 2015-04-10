b770344582ffced6034f1d306fe193ff50e490bc
ashenoi <ashshenoi8@gmail.com>
2015-04-10 01:31:06 -0400

    Uncommenting

--- main.m
+++ main.m
@@ -1,42 +1,42 @@
 % % Double tank
-% close all;
-% clear all;
-% [X,U,J_array,U_star,T] = double_tank_problem_proj();
-% global r;
-% plot(T,U+1,T,U_star+1);
-% title('Relaxed Control and U\_star');
-% legend('U','U*')
-% saveas(gcf,'1_relaxed_control.png');
-% plot(T,X(2,:),T,r,T,X(1,:));
-% title('State vs Time');
-% legend('x2','ref','x1');
-% saveas(gcf,'1_state_vs_time.png');
-% 
-% plot(J_array);
-% title('Cost vs iteration');
-% saveas(gcf,'1_cost.png');
+ close all;
+ clear all;
+ [X,U,J_array,U_star,T] = double_tank_problem_proj();
+ global r;
+ plot(T,U+1,T,U_star+1);
+ title('Relaxed Control and U\_star');
+ legend('U','U*')
+ saveas(gcf,'1_relaxed_control.png');
+ plot(T,X(2,:),T,r,T,X(1,:));
+ title('State vs Time');
+ legend('x2','ref','x1');
+ saveas(gcf,'1_state_vs_time.png');
+ 
+ plot(J_array);
+ title('Cost vs iteration');
+ saveas(gcf,'1_cost.png');
 
 
 % Robot path planning 
-% close all;
-% clear all;
-% [X,U,J_array,U_star,T] = robot_path_planning();
-% plot(T,U(1,:),T,U(2,:),T,1-U(1,:)-U(2,:));
-% title('Relaxed mode');
-% legend('fg','fcl','fcc')
-% saveas(gcf,'2_relaxed_mode.png');
-% plot(T,U_star(1,:),T,U_star(2,:),T, 1-U_star(1,:)-U_star(2,:));
-% title('Optimal mode');
-% legend('fg','fcl','fcc')
-% saveas(gcf,'2_optimal_mode.png');
-% plot(X(1,:),X(2,:));
-% title('State Trajectory');
-% 
-% saveas(gcf,'2_state_traj.png');
-% 
-% plot(J_array);
-% title('Cost vs iteration');
-% saveas(gcf,'2_cost.png');
+ close all;
+ clear all;
+ [X,U,J_array,U_star,T] = robot_path_planning();
+ plot(T,U(1,:),T,U(2,:),T,1-U(1,:)-U(2,:));
+ title('Relaxed mode');
+ legend('fg','fcl','fcc')
+ saveas(gcf,'2_relaxed_mode.png');
+ plot(T,U_star(1,:),T,U_star(2,:),T, 1-U_star(1,:)-U_star(2,:));
+ title('Optimal mode');
+ legend('fg','fcl','fcc')
+ saveas(gcf,'2_optimal_mode.png');
+ plot(X(1,:),X(2,:));
+ title('State Trajectory');
+ 
+ saveas(gcf,'2_state_traj.png');
+ 
+ plot(J_array);
+ title('Cost vs iteration');
+ saveas(gcf,'2_cost.png');
 
 close all;
 clear all;
